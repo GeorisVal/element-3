880c66a15854bbe88db307fc5267495f6d6de28b
GeorisVal <108730360+GeorisVal@users.noreply.github.com>
2022-08-01 20:43:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,38 +4,122 @@ function checkFields()
     let lastName = document.forms["form1"]["lastname"].value;
     let email = document.forms["form1"]["email"].value;
     let password = document.forms["form1"]["password"].value;
-    let spaces = /\s/;
-    let validEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    const spaces = /\s/;
+    const validEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,5})+$/;
+    const modal = document.getElementById("modal");
+    let modalText = document.getElementById("modal--text");
     if (firstName === "" || firstName.match(spaces)) {
-        /* alert('Please enter your first name !'); */
-        document.getElementById("form--firstname").style = "border: 2px solid rgb(255, 122, 122)";
         document.getElementById("form--firstname-empty").style = "visibility: visible";
+        document.getElementById("form--firstname").
+        setCustomValidity("Please enter your first name.")
     }
     if (lastName === "" || lastName.match(spaces)) {
-        /* alert ('Please enter your last name !'); */
-        document.getElementById("form--lastname").style = "border: 2px solid rgb(255, 122, 122)";
         document.getElementById("form--lastname-empty").style = "visibility: visible";
-    }
-    if (email === "" || email.match(spaces)) {
-        /* alert ('Please enter your email address !'); */
-        document.getElementById("form--email").style = "border: 2px solid rgb(255, 122, 122)"
-        document.getElementById("form--email-error").innerHTML = "Email address cannot be empty";
-        document.getElementById("form--email-error").style = "visibility: visible";
-    }
-    if (!email.match(validEmail) && email != "") {
-        /* alert("You have entered an invalid email address !") */
-        document.getElementById("form--email-error").innerHTML = "It looks like this is an invalid email address.";
-        document.getElementById("form--email-error").style = "visibility: visible";
-        document.getElementById("form--email").style = "border: 2px solid rgb(255, 122, 122)";
+        document.getElementById("form--lastname").
+        setCustomValidity("Please enter your last name.")
     }
     if (password === "" || password.match(spaces)) {
-        /* alert ('Please enter your password !'); */
-        document.getElementById("form--password").style = "border: 2px solid rgb(255, 122, 122)";
         document.getElementById("form--password-empty").style = "visibility: visible";
-        return false;
+        document.getElementById("form--password").
+        setCustomValidity("Please enter your password.")
+    }
+    if (email === "" || email.match(spaces)) {
+        document.getElementById("form--email-error").style = "visibility: visible";
+        document.getElementById("form--email-error").innerHTML = "Email cannot be empty";
+        document.getElementById("form--email").
+        setCustomValidity("Please enter your email.");
+        return false
+    }
+    if (!email.match(validEmail) && email === "") {
+        document.getElementById("form--email-error").innerHTML = "It looks like this is an invalid email address.";
+        document.getElementById("form--email-error").style = "visibility: visible";
+        return false
     }
     {
-        alert('Thank you, ' + firstName + '. You should receive an email at ' + email + '.');
+    if(password != "" && email != "" && email && lastName != "" && firstName != "")
+        modalText.innerHTML = "Thank you for registering, " + firstName + " ! You should receive an email at " + email + " shortly. You will then enjoy 7 days of Premium services free of charge !"
+        modal.style.display = "Block"
         return true;
+    }
+}
+
+function closeModal()
+{
+    const modal = document.getElementById("modal");
+    modal.style.display = "none";
+}
+
+function checkFirstName()
+{
+    let firstName = document.forms["form1"]["firstname"].value;
+    let spaces = /\s/;
+    if (firstName === "" || firstName.match(spaces)) {
+        document.getElementById("form--firstname-empty").style = "visibility: visible";
+        document.getElementById("form--firstname").
+        setCustomValidity("Please enter your first name.")
+        return false;
+    }
+    else {
+        document.getElementById("form--firstname-empty").style = "visibility: collapsed";
+        document.getElementById("form--firstname").
+        setCustomValidity("")
+        return true
+    }
+}
+
+function checkLastName()
+{
+    let lastName = document.forms["form1"]["lastname"].value;
+    let spaces = /\s/;
+    if (lastName === "" || lastName.match(spaces)) {
+        document.getElementById("form--lastname-empty").style = "visibility: visible";
+        document.getElementById("form--lastname").
+        setCustomValidity("Please enter your last name.")
+        return false;
+    }
+    else {
+        document.getElementById("form--lastname-empty").style = "visibility: collapsed";
+        document.getElementById("form--lastname").
+        setCustomValidity("")
+        return true
+    }
+}
+
+function checkEmail()
+{
+    let email = document.forms["form1"]["email"].value;
+    let spaces = /\s/;
+    let validEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,5})+$/;
+    if (email === "" || email.match(spaces)) {
+        document.getElementById("form--email-error").style = "visibility: visible";
+        document.getElementById("form--email-error").innerHTML = "Email cannot be empty";
+        document.getElementById("form--email").
+        setCustomValidity("Please enter your email.")
+        return false;
+    }
+    if (!email.match(validEmail) && email != "") {
+        document.getElementById("form--email-error").innerHTML = "It looks like this is an invalid email address.";
+        document.getElementById("form--email-error").style = "visibility: visible";
+        return false;
+    }
+    else {
+        document.getElementById("form--email-error").style = "visibility: collapsed";
+        document.getElementById("form--email").setCustomValidity("")
+        return true
+    }
+}
+
+function checkPassword()
+{
+    let password = document.forms["form1"]["password"].value;
+    if (password === "") {
+        document.getElementById("form--password-empty").style = "visibility: visible";
+        document.getElementById("form--password").setCustomValidity("Please enter your password.")
+        return false;
+    }
+    else {
+        document.getElementById("form--password-empty").style = "visibility: collapsed";
+        document.getElementById("form--password").setCustomValidity("")
+        return true
     }
 }
